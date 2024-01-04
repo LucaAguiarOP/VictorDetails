0f88769618e2fe3973bae94f014aef86e97bf86a
Luca Aguiar Oliveira Paiva <lucapaiva84@gmail.com>
2024-01-03 21:55:48 -0300

    Victor Logo Added

--- VictorDetails/ContentView.swift
+++ VictorDetails/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Image("victorlogo")
+                .imageScale(.small)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
